092b92f3d8b6366230afc2a007c714e1f6a720b7
Nick Gangloff <nickgangloff@gmail.com>
2013-03-25 14:03:01 -0500

    added arm move methods

--- src/development/DemoRemediation.java
+++ src/development/DemoRemediation.java
@@ -98,19 +98,153 @@ public class DemoRemediation
 	}
 	
 	// Returns String with sensor move location
-	private void moveArmDown(String challenge)
-	{
-		if (challenge.equals("Below Ground"))
-		{
+		//the current values are rough calculations as the exact measurements of the robot are not known.
+		
+		//BECAUSE we cannot measure the height of the water with the robot during its independent
+		//naviagtion, we must hard code the different angles for each case. therefore, any numbers changed here, change it
+		//in the method below too (the method to removeSensors). just remember to change both values. note that the 
+		//removeSensors method is the reverse order of these current algorithm steps. 
+		String moveSensor(int caseNum)
+		{// second method to remove the sensors
 			
+			//bellow ground, robot needs to be about 29.2 cm away from the center of the water.
+			if(caseNum == 1)
+			{
+				//these forloops are to get the arm in the correct general location, the closer
+				//it gets to the water, the more slowly the arm will move, to help avoid accidents
+				for(int i = 11; i>0; i--)
+				{
+				r.moveBothServos(0, i);//move second arm into correct location, 66 degrees
+				r.sleep(200);
+				}
+				for(int i = 19; i>0; i--) 
+				{
+				r.moveBothServos(i, 0); // moves main arm directly over the water, 155 degrees
+				r.sleep(200);
+				}
+				
+			}
+			
+			//ground.  since the water is at ground level. there shouldnt be a need to take 
+			//multiple steps to angle the sensor perpendicularly over. However, the robot 
+			//needs to be 29.28 cm from the water in order for this to work.
+			if(caseNum == 2)
+			{
+				//these forloops are to get the arm in the correct general location, the closer
+				//it gets to the water, the more slowly the arm will move, to help avoid accidents
+				for(int i = 14; i>0; i--)
+				{
+				r.moveBothServos(0, i);//104
+				r.sleep(200);
+				}
+				for(int i = 13; i>0; i--) 
+				{
+				r.moveBothServos(i, 0); // moves main arm directly over the water, 91 degrees
+				r.sleep(200);
+				}
+			}
+			
+			
+			//above ground
+			if(caseNum == 3)
+			{
+				//these forloops are to get the arm in the correct general location, the closer
+				//it gets to the water, the more slowly the arm will move, to help avoid accidents
+				for(int i = 11; i>0; i--)
+				{
+				r.moveBothServos(0, i);//move second arm into extended position, 66 degrees (plus the 25 the arm is already at)
+				r.sleep(200);
+				}
+				for(int i = 11; i>0; i--) 
+				{
+				r.moveBothServos(i, 0); // moves main arm directly over the water, 66 degrees
+				r.sleep(200);
+				}
+				for(int i = 10; i>0; i--)
+				{
+				r.moveBothServos(0, -i);// -55 degrees, will angle the sensor over the water.
+				r.sleep(200);
+				}
+				for(int i = 3; i>0; i--) 
+				{
+				r.moveBothServos(i, 0); // very slight movement to put the sensor into the water
+				r.sleep(200);
+				}
+				
+			}
+				
+			return "Sensors deployed";
 		}
-	    	else if (challenge.equals("Above Ground"))
-	    	{
-	  	  	
-	    	}
-	    	else if (challenge.equals("Ground Level"))
-	    	{
-	    	
-	   	}
-	}
+		
+		String removeSensor(int caseNum)
+		{// reverse of the moveSensor method
+			
+		
+			if(caseNum == 1)
+			{
+				//reverse steps to remove the sensors. 
+				for(int i = 19; i>0; i--) 
+				{
+				r.moveBothServos(-i, 0); 
+				r.sleep(200);
+				}
+				for(int i = 11; i>0; i--)
+				{
+				r.moveBothServos(0, -i);
+				r.sleep(200);
+				}
+				
+				
+			}
+			
+			
+			if(caseNum == 2)
+			{
+				
+				for(int i = 13; i>0; i--) 
+				{
+				r.moveBothServos(-i, 0); 
+				r.sleep(200);
+				}
+				for(int i = 14; i>0; i--)
+				{
+				r.moveBothServos(0, -i);
+				r.sleep(200);
+				}
+				
+			}
+			
+			
+			//above ground
+			if(caseNum == 3)
+			{
+				
+				for(int i = 3; i>0; i--) 
+				{
+				r.moveBothServos(-i, 0); 
+				r.sleep(200);
+				}
+				for(int i = 10; i>0; i--)
+				{
+				r.moveBothServos(0, i);
+				r.sleep(200);
+				}
+				for(int i = 11; i>0; i--) 
+				{
+				r.moveBothServos(-i, 0); 
+				r.sleep(200);
+				}
+				for(int i = 11; i>0; i--)
+				{
+				r.moveBothServos(0, -i);
+				r.sleep(200);
+				}
+				
+				
+				
+				
+			}
+				
+			return "Sensors retracted";
+		}
 }
