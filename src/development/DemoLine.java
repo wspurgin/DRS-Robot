c0de53b031d2175a0cc1abd775084734ca5fc2ae
Will Spurgin <will@rewardStyle.com>
2013-04-05 16:27:53 -0500

    oops, last minute. We forgot this

--- src/development/DemoLine.java
+++ src/development/DemoLine.java
@@ -23,22 +23,22 @@ public class DemoLine {
 		r.setPort(this.port);
 		r.setHasEncodedMotors(true);
 		r.connect();
-	 
-		r.refreshAnalogPins(); // Cache the Analog pin information 
-		AnalogPin bumpSensor = r.getAnalogPin(bumpSensorPinNumber);
-//       Will move (some what more slowly than normal because of the loop) until bump sensor is triggered.
         
-		while(bumpSensor.getValue() != 0)
+		r.refreshAnalogPins(); // Cache the Analog pin information
+		
+        //       Will move (some what more slowly than normal because of the loop) until bump sensor is triggered.
+		r.runMotor(RXTXRobot.MOTOR1, 255, RXTXRobot.MOTOR2, 255, 0);
+		while(r.getAnalogPin(this.bumpSensorPinNumber).getValue() != 0)
 		{
-			r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 2000, RXTXRobot.MOTOR2, 255, 2000);
 			r.refreshAnalogPins();
+			System.out.println(r.getAnalogPin(this.bumpSensorPinNumber).getValue());
 		}
-//       After bump sensor is triggered, Robot will wait two seconds, then back up slightly and make
-//        a 90 degree left turn. NOTE: ticks chosen arbitrarily for now.
+		r.runMotor(RXTXRobot.MOTOR1, 0, RXTXRobot.MOTOR2, 0, 0);
+        //       After bump sensor is triggered, Robot will wait two seconds, then back up slightly and make
+        //        a 90 degree left turn. NOTE: ticks chosen arbitrarily for now.
 		r.sleep(2000);
 		r.runEncodedMotor(RXTXRobot.MOTOR1, -255, 5000, RXTXRobot.MOTOR2, -255, 5000);
-		r.runEncodedMotor(RXTXRobot.MOTOR1, -255, 2055, RXTXRobot.MOTOR2, 255, 2055);
-		r.close(); 
+		r.runEncodedMotor(RXTXRobot.MOTOR1, -255, 2472, RXTXRobot.MOTOR2, 255, 2472);
 	}
 	
 	public void close()
