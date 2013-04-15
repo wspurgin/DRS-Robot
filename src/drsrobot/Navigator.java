f388e93f6cbad4c3acf9839223a5ef4e7c875055
SZanetti <szanetti@smu.edu>
2013-04-15 15:35:21 -0500

    Wrote findRFID() and added courseNumber variable

--- src/drsrobot/Navigator.java
+++ src/drsrobot/Navigator.java
@@ -6,7 +6,10 @@ import rxtxrobot.*;
 public class Navigator
 {
 	private RXTXRobot r;
+	private RFIDSensor sensor;
+	private final String RFID_PORT = "/dev/tty.usbserial-A901JX0L";
 	private boolean bumpSensorEngaged;
+	private int courseNumber;
 	private final int NORTH = 90;
 	private final int EAST = 0;
 	private final int WEST = 180;
@@ -15,11 +18,13 @@ public class Navigator
 	public Navigator(RXTXRobot r)
 	{
 		this.r = r;
+		this.sensor = new RFIDSensor();
+		this.sensor.setPort(RFID_PORT);
 		this.bumpSensorEngaged = false;
 	}
 	public void setUp()
 	{
-		orient(this.NORTH);
+		this.orient(this.NORTH);
 	}
 	public void orient(int direction)
 	{
@@ -34,12 +39,12 @@ public class Navigator
 			this.r.runMotor(RXTXRobot.MOTOR1, 255, RXTXRobot.MOTOR2, 255, 1000);
 		else
 			this.bumpSensorEngaged = false;
-		r.refreshAnalogPins();
-		while(r.readCompass() != direction)
+		this.r.refreshAnalogPins();
+		while(this.r.readCompass() != direction)
 		{
-			if(readBumpSensor(1) || readBumpSensor(2))
+			if(this.readBumpSensor(1) || this.readBumpSensor(2))
 			{
-				bumpSensorEngaged = true;
+				this.bumpSensorEngaged = true;
 				break;
 			}
 			this.r.refreshAnalogPins();
@@ -48,40 +53,83 @@ public class Navigator
 		if(this.bumpSensorEngaged)
 		{
 			this.bumpSensorEngaged = false;
-			r.refreshAnalogPins();
-			orient(direction);
+			this.r.refreshAnalogPins();
+			this.orient(direction);
 		}
 	}
+//	This method circles the perimeter of the playing field until the robot locates
+//	the RFID tag and assigns the courseNumber appropriately.
 	public void findRFID()
 	{
-		
+		this.sensor.connect();
+		this.r.runMotor(RXTXRobot.MOTOR1, 255, RXTXRobot.MOTOR2, 255, 0);
+		while(!this.sensor.hasTag())
+		{
+			if(this.readBumpSensor(1) || this.readBumpSensor(2))
+			{
+				this.bumpSensorEngaged = true;
+				break;
+			}
+			this.r.refreshAnalogPins();
+		}
+		this.r.runMotor(RXTXRobot.MOTOR1, 0, RXTXRobot.MOTOR2, 0, 0);
+		if(this.bumpSensorEngaged)
+		{
+			this.bumpSensorEngaged = false;
+			this.r.refreshAnalogPins();
+			int bearing = this.r.readCompass();
+			if(bearing == EAST)
+				this.orient(SOUTH);
+			else
+				this.orient(bearing - 90);
+			this.findRFID();
+		}
+		String tag = this.sensor.getTag();
+		if(tag.equals("6A003E4EA6BC"))
+			this.courseNumber = 1;
+		else if(tag.equals("6A003E834B9C"))
+			this.courseNumber = 2;
+		else if(tag.equals("6A003E6E477D"))
+			this.courseNumber = 3;
+		else
+			System.out.println("ERROR: Invalid RFID tag");
 	}
 	public void goToWell()
 	{
 		
 	}
-	public RXTXRobot getR() {
+	public RXTXRobot getR() 
+	{
 		return r;
 	}
-	public void setR(RXTXRobot r) {
+	public void setR(RXTXRobot r) 
+	{
 		this.r = r;
 	}
-	public int getNORTH() {
+	public int getNORTH() 
+	{
 		return NORTH;
 	}
-	public int getEAST() {
+	public int getEAST() 
+	{
 		return EAST;
 	}
-	public int getWEST() {
+	public int getWEST() 
+	{
 		return WEST;
 	}
-	public int getSOUTH() {
+	public int getSOUTH() 
+	{
 		return SOUTH;
+	}
+	public int getCourseNumber()
+	{
+		return this.courseNumber;
 	}
 	private void moveForwardWithBumpSensors()
 	{
 		this.r.runMotor(RXTXRobot.MOTOR1, 255, RXTXRobot.MOTOR2, 255, 0);
-		while(!readBumpSensor(1) && !readBumpSensor(2))
+		while(!this.readBumpSensor(1) && !this.readBumpSensor(2))
 		{
 			this.r.refreshAnalogPins();
 		}
